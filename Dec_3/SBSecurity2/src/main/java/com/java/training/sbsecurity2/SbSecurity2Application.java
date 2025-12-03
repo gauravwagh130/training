f1102b17658a492c89bed8e5bd1b77801338a59e
gauravwagh130 <gauravwagh130@gmail.com>
2025-12-03 13:09:07 +0530

    SpringBoot Security Sample Project

---
+++ Dec_3/SBSecurity2/src/main/java/com/java/training/sbsecurity2/SbSecurity2Application.java
@@ -0,0 +1,13 @@
+package com.java.training.sbsecurity2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSecurity2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbSecurity2Application.class, args);
+    }
+
+}
---
+++ Dec_3/SBSecurity2/src/test/java/com/java/training/sbsecurity2/SbSecurity2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.training.sbsecurity2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbSecurity2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
