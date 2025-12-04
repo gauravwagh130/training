e83d5af1e13793b345d5431b391dd4b334b56861
gauravwagh130 <gauravwagh130@gmail.com>
2025-12-04 11:21:12 +0530

    Create Eureka Server

---
+++ Dec_4/EurekaClient/src/main/java/com/java/training/HelloController.java
@@ -0,0 +1,17 @@
+package com.java.training;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping(value="/")
+    public String show() {
+        return "Welcome to Spring Boot...";
+    }
+
+    @GetMapping(value="/test")
+    public String test() {
+        return "Test Restful Web Service...";
+    }
+}
