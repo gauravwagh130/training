c72154be9f4daaa60098dd0762315ac84b253c59
gauravwagh130 <gauravwagh130@gmail.com>
2025-12-08 23:01:19 +0530

    GitHub OAuth

---
+++ Dec_8/SBOAUTH/src/test/java/com/java/training/sboauth/SboauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.training.sboauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SboauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
