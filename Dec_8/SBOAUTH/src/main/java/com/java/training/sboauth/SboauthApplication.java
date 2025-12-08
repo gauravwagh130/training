de02d361e18224a6a024e8e363aba6a2baeef189
gauravwagh130 <gauravwagh130@gmail.com>
2025-12-08 22:26:01 +0530

    Adds OAuth

---
+++ Dec_8/SBOAUTH/src/main/java/com/java/training/sboauth/SboauthApplication.java
@@ -0,0 +1,13 @@
+package com.java.training.sboauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SboauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SboauthApplication.class, args);
+    }
+
+}
