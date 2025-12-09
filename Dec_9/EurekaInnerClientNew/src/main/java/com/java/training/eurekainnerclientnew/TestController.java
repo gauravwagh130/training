e0ba433eec2c2ed62d03b2198b631dc58055a4f6
gauravwagh130 <gauravwagh130@gmail.com>
2025-12-09 13:16:05 +0530

    Implements Github OAuth

--- Dec_9/EurekaInnerClientNew/src/main/java/com/java/training/eurekainnerclientnew/TestController.java
+++ Dec_9/EurekaInnerClientNew/src/main/java/com/java/training/eurekainnerclientnew/TestController.java
@@ -1,16 +1,25 @@
 package com.java.training.eurekainnerclientnew;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class TestController {
+
     @Autowired
     private RestTemplate restTemplate;
+
+    @GetMapping(value="/")
+    public String home() {
+        return "Home — <a href='/oauth2/authorization/github'>Login With GitHub</a>";
+    }
+
     @GetMapping(value = "/showAgent")
-    public Object showEmploy() {
+    public Object showAgent(@AuthenticationPrincipal OAuth2User user) {
         return restTemplate.getForObject("http://AGENTCLIENT/showAllAgent", Object.class);
     }
 
